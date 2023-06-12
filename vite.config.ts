2254d41c42e098b0dea27578235e43f5057e1802
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-06-12 14:21:36 +0800

    [config] change back svelte and vite config back
    Description
    Since those changes have also been reverted
    posts alias not needed, can't be used

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ export const defaultConfig: UserConfigExport = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
+	}
 };
 export default defineConfig(defaultConfig);
